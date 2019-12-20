572584cc9b8065129ce5be06ead3c197b72a54f5
Gor Mkhitaryan <gor@difocus.ru>
2019-12-21 02:34:34 +0300

    Implemented variable creating functionality

---
+++ src/Puff/Compilation/Element/SetElement.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Puff\Compilation\Element;
+
+use Puff\Compilation\Services\VariableTransliterator;
+
+class SetElement extends AbstractElement
+{
+
+    /**
+     * @param array $attributes
+     * @return mixed
+     */
+    public function process(array $attributes)
+    {
+        return sprintf("<?php %s = %s; ?>",
+                VariableTransliterator::transliterate($attributes['new-variable']),
+                VariableTransliterator::transliterate($attributes['value']));
+    }
+
+    public function handleAttributes($tokenAttributes)
+    {
+        return ["new-variable" => $tokenAttributes[0], 'value' => $tokenAttributes[2]];
+    }
+}
