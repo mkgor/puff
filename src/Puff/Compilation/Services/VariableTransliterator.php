18489ba03282b99a7d53fbf18f0c7e239294f059
Gor Mkhitaryan <gor@difocus.ru>
2019-12-21 02:38:22 +0300

    Logic improvements

--- src/Puff/Compilation/Services/VariableTransliterator.php
+++ src/Puff/Compilation/Services/VariableTransliterator.php
@@ -14,8 +14,11 @@ class VariableTransliterator
      */
     public static function transliterate($variableString)
     {
-        preg_replace_callback('/\.(?<var>\w+)/', function ($result) {
-            return $result['var'];
-        }, $variableString);
+        $variable = preg_replace('/^([a-zA-Z][a-zA-Z0-9])/', '\$$0', $variableString);
+
+        /** Checking, if provided string is variable and if there are calling array item, it transliterates it into PHP syntax */
+        return preg_replace_callback('/([\'"]).*\1(*SKIP)(*F)|\.(?<value>\w+)/', function ($result) {
+            return sprintf("['%s']", trim($result['value']));
+        }, $variable);
     }
 }
