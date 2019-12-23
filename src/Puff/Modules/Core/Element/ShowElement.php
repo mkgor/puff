75b38f6d6e193e81ba7d9534d52c19227f8a6d38
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2019-12-23 22:54:48 +0300

    Deleted whitespace

--- src/Puff/Modules/Core/Element/ShowElement.php
+++ src/Puff/Modules/Core/Element/ShowElement.php
@@ -37,6 +37,6 @@ class ShowElement extends AbstractElement
         }
 
         /** Building PHP snippet which will display data from variable  */
-        return sprintf("%s <?php echo %s; ?>", $filtersString, $compiledVariable);
+        return sprintf("%s<?php echo %s; ?>", $filtersString, $compiledVariable);
     }
 }
