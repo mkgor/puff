0dc679e98bb94c28c0bf6767b7b6c8252e078b11
Gor Mkhitaryan <mkhitaryan.gor@inbox.ru>
2019-12-27 10:32:11 +0300

    Implemented using PHP functions as filter

--- src/Puff/Compilation/Service/FilterStringBuilder.php
+++ src/Puff/Compilation/Service/FilterStringBuilder.php
@@ -16,6 +16,7 @@ class FilterStringBuilder
      * @param $compiledVariable
      * @return array
      * @throws InvalidFilterException
+     * @throws \Exception
      */
     public function buildString(array $filters, $compiledVariable)
     {
@@ -27,16 +28,19 @@ class FilterStringBuilder
         foreach ($filters as $filter) {
             $arguments = null;
 
-            if(strpos($filter, '(')) {
+            if (strpos($filter, '(')) {
                 preg_match_all('/(?<name>\w+)|\((?<arguments>(.*))\)/', $filter, $matches, PREG_SET_ORDER, 0);
 
                 $filter = $matches[0]['name'];
                 $arguments = ',' . $matches[1]['arguments'];
             }
 
-            if(isset($registeredFiltersList[$filter])) {
-                $newCompiledVariable = '$tmp_'.random_int(0,10000);
-                $filtersString .= $newCompiledVariable . ' = ' . $registeredFiltersList[$filter] . '::handle(' . $compiledVariable . $arguments .');' . PHP_EOL;
+            if (isset($registeredFiltersList[$filter])) {
+                $newCompiledVariable = $this->generateTmpVariable();
+                $filtersString .= $newCompiledVariable . ' = ' . $registeredFiltersList[$filter] . '::handle(' . $compiledVariable . $arguments . ');' . PHP_EOL;
+            } else if(function_exists($filter)) {
+                $newCompiledVariable = $this->generateTmpVariable();
+                $filtersString .= $newCompiledVariable . ' = ' . $filter . '(' . $compiledVariable . $arguments . ');' . PHP_EOL;
             } else {
                 ob_end_clean();
 
@@ -47,5 +51,14 @@ class FilterStringBuilder
         $filtersString .= ' ?>';
 
         return ['tmp_variable' => $newCompiledVariable, 'compiled' => $filtersString];
+    }
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    private function generateTmpVariable()
+    {
+        return '$tmp_' . random_int(0, 10000);
     }
 }
